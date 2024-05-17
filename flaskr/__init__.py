187ea591d95fa21a3eea9b88cd3d95df7edb18cb
santiago martinez <martinezvelez2@gmail.com>
2024-05-17 17:42:31 +0000

    Descrpción de los cambios realizados

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route('/home')
-def hello():
-    return "Hello, World y a todos!"
+@app.route('/index')
+def index():
+    user_dir = request.remote_addr
+    response = make_response(redirect("/show_address_info"))
+    response.set_cookie("user_ip_info", user_dir)
+    return response
+    
+@app.route("/show_address_info")
+def show_info():
+    user_ip = request.cookies.get("user_ip_info")
+    return render_template("ip_info.html", user_ip=user_ip)
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port= 3000)
+    app.run(host='0.0.0.0', port= 3000, debug=True)
