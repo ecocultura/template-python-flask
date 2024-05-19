db90a1f52e28c0cd4dc39c79793cd9b2cc8daa46
santiago martinez <martinezvelez2@gmail.com>
2024-05-19 06:43:32 +0000

    Descripción de los cambios realizados

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap  #cada que se inicie hay que instalar --- pip install flask-bootstrap
+from flask_wtf import FlaskForm #cada que se inicie hay que instalar --- pip install flask-wtf
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 Bootstrap = Bootstrap(app)
@@ -7,6 +10,11 @@ app.config["SECRET_KEY"] = 'CLAVE SEGURA'
 
 
 items = ["ITEM 1", "ITEM 2", "ITEM 3", "ITEM 4"]
+
+class LoginForm(FlaskForm):
+    username = StringField("nombre del usuario", validators=[DataRequired()])
+    password = PasswordField("contraseña", validators=[DataRequired()])
+    submit = SubmitField("enviar datos", validators=[DataRequired()])
 
 @app.errorhandler(404)
 def not_found_endpint(error):
@@ -22,9 +30,11 @@ def index():
 @app.route("/show_address_info")
 def show_info():
     user_ip = session.get("user_ip_info")
+    login_form = LoginForm()
     context = {
         "user_ip":user_ip,
-        "items":items
+        "items":items,
+        "login_form":login_form
     }
     return render_template("ip_info.html", **context)
     #**context simplemente desbarata el diccionario context y permite tomar las claves como variables
